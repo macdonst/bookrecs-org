2c7314ad3828b98020a766a25658d688f6efc7cf
macdonst <simon.macdonald@gmail.com>
2023-03-21 18:08:50 -0400

    Remove unused classes
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/elements/site-header.mjs
+++ app/elements/site-header.mjs
@@ -1,12 +1,14 @@
 export default function SiteHeader ({ html }) {
   return html`
-    <h1 class='text2 text4-lg text-center font-heading font-bold tracking-2 pt4 pt6-lg pb0 pb2-lg'>
-      <a href='/' class='no-underline'>
-        Book Recs by Simon MacDonald
-      </a>
-    </h1>
-    <p class='text-center font-body pb4 pb6-lg'>
-      Your human goodreads
-    </p>
+    <header class='pl0 pr0 pl5-lg pr5-lg'>
+      <h1 class='text2 text4-lg text-center font-heading font-bold tracking-1 pt4 pt6-lg pb0 pb2-lg'>
+        <a href='/' class='no-underline'>
+          Book Recs by Simon MacDonald
+        </a>
+      </h1>
+      <p class='text-center font-body pb4 pb6-lg'>
+        Your human goodreads
+      </p>
+    </header>
   `
 }
