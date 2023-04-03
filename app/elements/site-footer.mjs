61525a2d030d3b1103993dd5f92134f1be111cfd
macdonst <simon.macdonald@gmail.com>
2023-04-03 16:58:15 -0400

    Add Mastodon link to footer
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/elements/site-footer.mjs
+++ app/elements/site-footer.mjs
@@ -1,6 +1,9 @@
 export default function SiteFooter ({ html }) {
   return html`
     <footer class='pt4 pt6-lg pb4 pb6-lg'>
+      <p class='text-center pb-4'>
+        <a rel='me' href='https://mastodon.online/@macdonst' class='font-body text-1 uppercase tracking3 underline'>Mastodon</a>
+      </p>
       <p class='text-center'>
         <a href='/rss' class='font-body text-1 uppercase tracking3 underline'>RSS</a>
       </p>
